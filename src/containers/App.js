3423a5181384f956fccb014530b45d7efd93162c
Thomas Kennedy <thomasmatthewkennedy@gmail.com>
2019-05-15 22:59:13 -0500

    backed out character, validation components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import styles from './App.module.css';
 import Cockpit from '../components/Cockpit/Cockpit';
 import Persons from '../components/Persons/Persons';
-import Validation from '../components/Validation/Validation';
-import CharComponent from '../components/Char/Char';
 
 class App extends Component {
   state = {
@@ -12,8 +9,7 @@ class App extends Component {
       { id: '002', name: "Cyrus", age: 200 },
       { id: '003', name: "Ozymandias", age: 52 },
     ],
-    showPersons: false,
-    textValue: ''
+    showPersons: false
   }
 
   deletePersonHandler = (id) => {
@@ -41,33 +37,11 @@ class App extends Component {
     })
   }
 
-  reversePersonsHandler = () => {
-    this.setState(state => ({
-      ...state,
-      persons: state.persons.reverse()
-    }));
-  }
-
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState(state => ({
       showPersons: !doesShow
     }));
-  }
-
-  onTextChange = (event) => {
-    const newTextValue = event.target.value;
-    this.setState({
-      textValue: newTextValue
-    })
-  }
-
-  onRemoveLetter = (index) => {
-    const textValues = this.state.textValue.split('');
-    textValues.splice(index, 1);
-    this.setState({
-      textValue: textValues.join('')
-    })
   }
 
   render() {
@@ -81,30 +55,13 @@ class App extends Component {
       />
     }
 
-    let charComponents = (
-      <div>
-        {
-          this.state.textValue.split('').map((letter, index) => {
-            return <CharComponent key={index} index={index} click={this.onRemoveLetter} value={letter} />
-          })
-        }
-      </div>
-    );
-
-
-
     return (
-      <div className={styles.App}>
+      <div>
         <Cockpit
           persons={this.state.persons}
           showPersons={this.state.showPersons}
           clicked={this.togglePersonsHandler} />
         {persons}
-        <hr />
-        <input type="text" onChange={(event) => this.onTextChange(event)} value={this.state.textValue} />
-        <p>{this.state.textValue}</p>
-        <Validation textLength={this.state.textValue.length} />
-        {charComponents}
       </div>
     );
   }
